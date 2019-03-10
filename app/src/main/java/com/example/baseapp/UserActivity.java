bb83e71e97caa8c3e512fc27d8ade951e2cbcabe
Nelma Barros <nbarros@uolinc.com>
2019-03-10 16:17:51 -0300

    Persistir User ao rotacionar tela

--- app/src/main/java/com/example/baseapp/UserActivity.java
+++ app/src/main/java/com/example/baseapp/UserActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +18,8 @@ public class UserActivity extends AppCompatActivity {
     private Button btSave;
 
     static final int REQUEST_CODE = 2;
+    static final String NAME = "NAME";
+    static final String PASSWORD = "PASSWORD";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,14 +48,37 @@ public class UserActivity extends AppCompatActivity {
                     Intent intent = new Intent(UserActivity.this, DrawerActivity.class);
                     intent.putExtra("NOME", user.getNome());
                     startActivityForResult(intent, REQUEST_CODE );
-
-//                    if(intent != null) {
-//                        intent.putExtra("NOME", user.getNome());
-//                        setResult(RESULT_OK, intent);
-//                        UserActivity.this.finish();
-//                    }
                 }
             }
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString(NAME, userName.getText().toString());
+        outState.putString(PASSWORD, userPassword.getText().toString());
+
+        User user =
+                new User(userName.getText().toString(), userPassword.getText().toString());
+
+        outState.putParcelable("USUARIO_PARCELABLE", user);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        String nome = savedInstanceState.getString(NAME);
+        String password = savedInstanceState.getString(PASSWORD);
+
+
+        userName.setText(nome);
+        userPassword.setText(password);
+
+        User user = savedInstanceState.getParcelable("USUARIO_PARCELABLE");
+        if (user != null) {
+            Log.d(getClass().getName(), user.getNome());
+        }
     }
 }
